0934b1c5b17a24faa28747bff781dbc9a765183e
Augustine udo Uzochukwu <auzochuk@f0r4s18.codam.nl>
2021-09-01 14:40:54 +0200

    Simplify map printing

--- solve.c
+++ solve.c
@@ -66,18 +66,12 @@ void	fill_square(struct s_map map, struct s_square biggest)
 
 void	print_map(struct s_map map)
 {
-	int	x;
 	int	y;
 
 	y = 0;
 	while (y < map.height)
 	{
-		x = 0;
-		while (x < map.width)
-		{
-			write(1, &map.map[y][x], 1);
-			++x;
-		}
+		write(1, map.map[y], map.width);
 		free(map.map[y]);
 		write(1, "\n", 1);
 		++y;
